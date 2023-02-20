06fa28783db59f127e4f6cb6cf45cb1f220f7d11
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-20 10:18:53 +0000

    Create Greeting component

---
+++ src/components/Greeting.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react';
+const Greeting = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+export default Greeting;
--- src/redux/service/api.js
+++ src/redux/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://127.0.0.1:3000/api/v1/messages';
+const url = 'http://127.0.0.1:3000/api/v1/greetings';
 
 const fetchMessage = async () => {
   const response = await axios.get(url);
